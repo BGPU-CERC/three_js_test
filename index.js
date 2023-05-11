d8ca622f25cc7009d115e0e441010aa4c691685e
John Doe <johndoe@example.com>
2023-05-11 18:56:55 +0900

    add: controls

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -17,12 +18,15 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 5, 5);
+controls.update();
 
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  controls.update();
   renderer.render(scene, camera);
 }
 
