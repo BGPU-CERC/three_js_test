3967c326c28d629d8e934636123189606fd90007
John Doe <johndoe@example.com>
2023-05-11 19:16:06 +0900

    add: load scene

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcccccc);
+scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -13,19 +17,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 5, 5);
 controls.update();
 
+const loader = new GLTFLoader();
+const gltf = await loader.loadAsync("test.glb");
+scene.add(gltf.scene);
+
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
+
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   controls.update();
   renderer.render(scene, camera);
 }
