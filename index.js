dbb7922ba2db09382f9868c8298301bc03fcdd5e
readandwrite864 <63188034+readandwrite864@users.noreply.github.com>
2023-05-20 17:07:55 +0900

    add: ik

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { CCDIKSolver } from "three/addons/animation/CCDIKSolver.js";
+import { TransformControls } from "three/addons/controls/TransformControls.js";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xcccccc);
@@ -18,19 +20,79 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 5, 5);
+camera.position.set(0, 10, 20);
 controls.update();
 
 const loader = new GLTFLoader();
 const gltf = await loader.loadAsync("test.glb");
 scene.add(gltf.scene);
 
-const light = new THREE.AmbientLight(0x404040); // soft white light
+let OOI = {};
+
+gltf.scene.traverse((n) => {
+  if (n.name === "Cube") {
+    OOI.mesh = n;
+  }
+
+  if (n.name.endsWith("_target")) {
+    OOI.target = n;
+  }
+});
+
+const iks = [
+  {
+    target: 4,
+    effector: 3,
+    links: [
+      {
+        index: 2,
+        rotationMin: new THREE.Vector3(0, 0, 0),
+        rotationMax: new THREE.Vector3(0, 0, 1),
+      },
+      {
+        index: 1,
+        rotationMin: new THREE.Vector3(0, 0, 0),
+        rotationMax: new THREE.Vector3(0, 0, 1),
+      },
+      {
+        index: 0,
+        rotationMin: new THREE.Vector3(0, 0, 0),
+        rotationMax: new THREE.Vector3(0, 0, 1),
+      },
+    ],
+  },
+];
+
+const ikSolver = new CCDIKSolver(OOI.mesh, iks);
+const ikHelper = ikSolver.createHelper();
+scene.add(ikHelper);
+
+const transformControls = new TransformControls(camera, renderer.domElement);
+transformControls.size = 0.75;
+transformControls.showX = true;
+transformControls.showY = true;
+transformControls.showZ = true;
+transformControls.space = "world";
+transformControls.attach(OOI.target);
+scene.add(transformControls);
+
+// disable orbitControls while using transformControls
+const setControls = (state) => (controls.enabled = state);
+transformControls.addEventListener("mouseDown", () => setControls(false));
+transformControls.addEventListener("mouseUp", () => setControls(true));
+
+const light = new THREE.AmbientLight(0x404040);
 scene.add(light);
+
+const size = 20;
+const divisions = 10;
+const gridHelper = new THREE.GridHelper(size, divisions);
+scene.add(gridHelper);
 
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
+  ikSolver.update();
   renderer.render(scene, camera);
 }
 
